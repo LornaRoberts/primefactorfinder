49f64b7301862bbfcb8d8740f2a990ca8a55d47e
Lorna <lroberts1980@yahoo.com>
2020-08-18 20:40:35 +0100

    Add files via upload

--- largestprimenumber.js
+++ largestprimenumber.js
@@ -1,15 +1,19 @@
 function prime(num){
-    for (let i=624797; i<num; i++){
+    for (let i= num-1; i>0; i--){
       if (num % i === 0){
+        //prints out largest prime factor first
         console.log(i);
-        i+= 1;
-  
+        i-= 1;
       }
+      
     }
+    //if prime number, only the original number and 1 will print out
+        console.log(num)
+      }
     
-   }
+   
     
     
-    prime(489652784103);
+    prime(15);
     
     
